fc5bf526df18064a57b07d9a36a42c8e50bcb398
Juan José <sedmirnel124@gmail.com>
2026-03-24 13:47:02 -0500

    Segunda versión del FirstAndFollow. funciona +-

--- firstAndFollows.py
+++ firstAndFollows.py
@@ -4,70 +4,94 @@ class Grammar():
         self.terminals: set[str] = terminals
         self.prodRules: set[tuple[str,str]] = prodRules
 
-def allNullable(prod: str, nullable: dict) -> bool:
+def allNullable(prod: str, nullable: dict[str,bool]) -> bool:
     for c in prod:
-        if c in nullable.keys():
-            if not nullable[c]:
-                return False
+        if not nullable[c]:
+            return False
     return True
 
-def firstAndFollows(G: Grammar) -> tuple[dict,dict,dict]:
-    first: dict = {}
-    follow: dict = {}
-    nullable: dict = {}
+def firstAndFollows(G: Grammar) -> tuple[dict[str, set],dict[str,set],dict[str,bool]]:
+    first: dict[str, set] = {}
+    follow: dict[str, set] = {}
+    nullable: dict[str, bool] = {}
     
     # Inicializaciones
 
     for c in G.nonTerminals | G.terminals:
-        nullable[c] = False
-        first[c] = {}
-        follow[c] = {}
+        nullable[c] = False # inicializar nullable a todos falsos
+
+        # inicializar first y follow como todos conjuntos facíos
+        first[c] = set()
+        follow[c] = set()
+
+    #print("DEBUG: ", nullable, first, follow, "\n")
     
     for rule in G.prodRules:
         if rule[1] == "":
             nullable[rule[0]] = True
+    
+    #print("DEBUG: ", nullable, "\n")
 
     for t in G.terminals: # el first de todo terminal es sí mismo
         first[t] = set(t)
+    
+    #print("DEBUG: ", first, "\n")
 
     # Guardar estados previos
-    prevFirst: dict = None
-    prevFollow: dict = None
-    prevNullable: dict = None
+    prevFirst: dict = {}
+    prevFollow: dict = {}
+    prevNullable: dict = {}
 
     firstIteration: bool = True
+    iterations: int = 0
     k: int = 0
 
     while firstIteration or (first != prevFirst and follow != prevFollow and nullable != prevNullable):
+        iterations += 1
+        print("Iteración: ", iterations, '\n')
         firstIteration = False
 
-        prevFirst = first
-        prevFollow = follow
-        prevNullable = nullable
+        prevFirst = first.copy()
+        prevFollow = follow.copy()
+        prevNullable = nullable.copy()
+
+        print(first, "\n", prevFirst)
 
         for rule in G.prodRules:
             k = len(rule[1])
+            print(rule, k)
 
-            if allNullable(rule[1], nullable):
+            if (k == 0) or allNullable(rule[1], nullable):
                 nullable[rule[0]] = True
 
             for i in range(k):
-                for j in range(i+1,k):
-                    if i == 0 or allNullable(rule[1][0:i-1], nullable):
-                        first[rule[0]] |= first[rule[1][i]]
+                print(i)
+                if i == 0 or allNullable(rule[1][0:i-1], nullable):
+                    print("FIRST[", rule[0], "] U FIRST[", rule[1][i], "]")
+                    first[rule[0]] |= first[rule[1][i]]
+                    print(first[rule[0]])
 
-                    if i == k or allNullable(rule[1][i+1:k], nullable):
-                        follow[rule[1][i+1]] |= follow[rule[0]]
+                if i == k-1 or allNullable(rule[1][i+1:k], nullable):
+                    print("FOLLOW[", rule[1][i], "] U FOLLOW[", rule[0], "]")
+                    follow[rule[1][i]] |= follow[rule[0]]
+                    print(follow[rule[1][i]])
                     
+                for j in range(i+1,k):
                     if i+1 == j or allNullable(rule[1][i+1:j-1], nullable):
+                        print("FOLLOW[", rule[1][i], "] U FIRST[", rule[1][j], "]")
                         follow[rule[1][i]] |= first[rule[1][j]]
+                        print(follow[rule[1][i]])
 
+        print(first, "\n", prevFirst)
     return first, follow, nullable
 
 def main():
-    G = Grammar({'X','Y','Z'}, {'a','c','d'}, {('Z',"d"), ('Z',"XYZ"), ('Y',""), ('Y',"c"), ('X',"Y"), ('X',"a")})
+    nt: set[str] = {'X','Y','Z'}
+    t: set[str] = {'a','c','d'}
+    pr: set[tuple[str,str]] = {('Z',"d"), ('Z',"XYZ"), ('Y',""), ('Y',"c"), ('X',"Y"), ('X',"a")}
+    G = Grammar(nt, t, pr)
 
-    result = firstAndFollows(G)
+    result: tuple[dict[str, set],dict[str,set],dict[str,bool]] = firstAndFollows(G)
 
     print("FIRST: ", result[0], "\n")
     print("FOLLOW: ", result[1], "\n")
